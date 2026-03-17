74c86389214e3213b9fac16f3e2d522b4a39a9ef
Harshvardhan <hkamble1@student.gsu.edu>
2026-03-16 22:21:07 -0400

    Connected all screens and set up routes, provider and theme in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,52 @@
-import 'package:flutter/material.dart';
+// lib/main.dart
 
-void main() {
-  runApp(const MainApp());
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/game_provider.dart';
+import 'services/puzzle_service.dart';
+import 'utils/app_theme.dart';
+import 'screens/home_screen.dart';
+import 'screens/mission_screen.dart';
+import 'screens/word_search_screen.dart';
+import 'screens/clue_tracker_screen.dart';
+import 'screens/achievements_screen.dart';
+import 'screens/leaderboard_screen.dart';
+import 'screens/settings_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PuzzleService.instance.loadPuzzles();
+  runApp(const EnigmaRoomsApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class EnigmaRoomsApp extends StatelessWidget {
+  const EnigmaRoomsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ChangeNotifierProvider(
+      create: (_) => GameProvider()
+        ..loadPrefs()
+        ..loadLeaderboard()
+        ..loadAchievements(),
+      child: Consumer<GameProvider>(
+        builder: (context, game, _) => MaterialApp(
+          title: 'ENIGMA ROOMS',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.light,
+          darkTheme: AppTheme.dark,
+          themeMode:
+              game.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          initialRoute: '/',
+          routes: {
+            '/':             (_) => const HomeScreen(),
+            '/mission':      (_) => const MissionScreen(),
+            '/wordsearch':   (_) => const WordSearchScreen(),
+            '/clues':        (_) => const ClueTrackerScreen(),
+            '/achievements': (_) => const AchievementsScreen(),
+            '/leaderboard':  (_) => const LeaderboardScreen(),
+            '/settings':     (_) => const SettingsScreen(),
+          },
         ),
       ),
     );
